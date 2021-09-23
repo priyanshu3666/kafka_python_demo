41a3beb5ea4d9c18e68a6bd45fd397d004038d96
Priyanshu Shukla <priyanshu.shukla@iedaas.com>
2021-09-23 15:50:41 +0530

    process consumed data

--- consumer4.py
+++ consumer4.py
@@ -1,6 +1,7 @@
 from kafka import KafkaConsumer
 import json
 
+from kafka.metrics.stats import rate
 
 if __name__ == '__main__':
     consumer = KafkaConsumer(
@@ -10,4 +11,25 @@ if __name__ == '__main__':
         group_id ='consumer_group_4')
     print("stating the consumer")
     for msg in consumer:
-        print("Bank rates  = {}".format(json.loads(msg.value))) 
+        fetched_list  = json.loads(msg.value)
+        rates = fetched_list.values()
+        if ['BOB_rates'] == fetched_list['PNB_rates'] or ['SBI_rates'] ==fetched_list['PNB_rates'] or ['SBI_rates'] ==fetched_list['BOB_rates']:
+            if fetched_list['BOB_rates'] == fetched_list['PNB_rates']:
+                if fetched_list['BOB_rates']>fetched_list['SBI_rates']:
+                    print("BOB gives equal  policy. to PNB  its rate is {}".format(fetched_list['BOB_rates']))
+                if fetched_list['SBI_rates'] ==fetched_list['BOB_rates']:
+                    print("SBI gives equal  policy. to PNB and BOB   its rate are {}".format(fetched_list['PNB_rates']))
+                    
+            if fetched_list['SBI_rates'] ==fetched_list['PNB_rates']:
+                if fetched_list['BOB_rates']<fetched_list['SBI_rates']:
+                    print("SBI gives equal  policy. to PNB  its rate is {}".format(fetched_list['SBI_rates']))
+        else:                
+            if fetched_list['BOB_rates']>fetched_list['PNB_rates'] and fetched_list['BOB_rates']>fetched_list['SBI_rates']:
+                    print("BOB gives best policy. its rate is {}".format(fetched_list['BOB_rates']))
+            else:
+                if fetched_list['SBI_rates']>fetched_list['PNB_rates']:
+                        print("SBI gives best policy. its rate is {}".format(fetched_list['SBI_rates']))
+                else :
+                        print("PNB gives best policy. its rate is {}".format(fetched_list['PNB_rates']))
+                
+        
